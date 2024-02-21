b36cefe62fc41f1a1a5690a880b5756a96ec283a
Elisaatnam <elisa.stueker@googlemail.com>
2024-02-21 09:47:40 +0100

    all basic functionalities done

--- src/component/TodoItem.tsx
+++ src/component/TodoItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Todo } from "../utils/model";
 import { RiEdit2Fill, RiDeleteBin6Line } from "react-icons/ri";
 
@@ -8,14 +9,64 @@ interface Props {
 }
 
 const TodoItem: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editedTodo, setEditedTodo] = useState<string>(todo.todo);
+
+  const markAsDone = (id: number): void => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number): void => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, todo: editedTodo } : todo
+      )
+    );
+    setEdit((prev) => !prev);
+  };
+
+  const handleDelete = (id: number): void => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="card w-96 bg-primary text-primary-content mb-3 relativ lg:m-3">
-      <RiDeleteBin6Line className="absolute top-4 right-4" />
-      <RiEdit2Fill className="absolute top-4 right-10" />
+      <RiDeleteBin6Line
+        onClick={() => handleDelete(todo.id)}
+        className="absolute top-4 right-4"
+      />
+      <RiEdit2Fill
+        onClick={() => setEdit((prev) => !prev)}
+        className="absolute top-4 right-10"
+      />
       <div className="card-body">
-        <h2 className="card-title">{todo.todo}</h2>
+        {edit ? (
+          <form onSubmit={(e) => handleEdit(e, todo.id)}>
+            <input
+              type="text"
+              value={editedTodo}
+              onChange={(e) => setEditedTodo(e.target.value)}
+            />
+          </form>
+        ) : (
+          <h2
+            className={`card-title ${
+              todo.isDone ? "line-through" : "no-underline"
+            }`}
+          >
+            {todo.todo}
+          </h2>
+        )}
+
         <div className="card-actions justify-end">
-          <button className="btn">Mark as done</button>
+          <button onClick={() => markAsDone(todo.id)} className="btn">
+            {todo.isDone ? "Mark as undone" : "Mark as done"}
+          </button>
         </div>
       </div>
     </div>
--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -10,7 +10,12 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
     <section className="lg:flex lg:flex-row lg:flex-wrap lg:items-center lg:justify-center">
       {todos.map((singleTodo) => (
-        <TodoItem key={singleTodo.id} todo={singleTodo} setTodos={setTodos} />
+        <TodoItem
+          key={singleTodo.id}
+          todo={singleTodo}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </section>
   );
